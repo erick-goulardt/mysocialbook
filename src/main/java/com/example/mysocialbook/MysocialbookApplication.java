cbcff600b7516c88cef7d4f6a3446adcc9c2dfbc
erickgoulardt <deverickgoulardt@gmail.com>
2023-09-25 12:38:14 -0300

    Primeiro commit

---
+++ src/main/java/com/example/mysocialbook/MysocialbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysocialbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysocialbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysocialbookApplication.class, args);
+	}
+
+}
